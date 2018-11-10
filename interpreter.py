21b7a85a55ce1140c597a9dc3913e0dc0e3feddd
Gilbert John Cuaresma <gilbert.tensorbros@gmail.com>
2018-11-11 01:31:08 +0800

    Comments

--- interpreter.py
+++ interpreter.py
@@ -11,7 +11,6 @@ from syntax import Syntax
 
 parsed_list = []
 
-
 def main():
     print('Welcome to IPOL interpreter!')
 
@@ -24,40 +23,51 @@ def main():
     # returns a 2d list containing the tokens per line of code
     tokens_list = tokenizer.tokenize(lines)
 
+    # create instance of the parser with the syntax declared in Syntax class
     parser = Parser(syntax=Syntax().get_syntax())
 
+    # iterate each line of the list containing the tokens
     for line in tokens_list:
         recursive_parse(parser, line, callback)
 
+    # print parsed list for debugging purposes
     for p in parsed_list:
         for t in p:
             print(t.type)
         print('**********')
+        
 
 
 def recursive_parse(parser, line, callback):
     parsed = parser.parse(line)
 
+    # parse the line
     if not equal_ignore_order(parsed, line):
         recursive_parse(parser, parsed, callback)
+        # if the line can no longer be parsed, the result will return an empty list
+        # thus, just return the previous unparsed list
         if len(parsed) == 0:
             callback(line)  
 
+    # the tokens have been parsed to a single statement ie. PRINT, ARITH_OP, etc
+    # convert these to STATEMENT or STATEMENTS for the final parsing
     elif len(parsed) == 1:
         parser = Parser(syntax=Syntax().get_final_syntax())
         parsed = parser.parse(parsed)
         callback(parsed)
+    # the parsing returned the same result ie. NUMBER can be parsed as NUMBER and so on
+    # just return the parsed list and end the loop
     else:
         parser = Parser(syntax=Syntax().get_syntax())
         parsed = parser.parse(parsed)
         callback(parsed)
         
-
-
+# callback function for getting the parsed list
 def callback(parsed):
     parsed_list.append(parsed)
 
-
+# checks if the returned parsed list is equal to the input list
+# which means the list can no longer be parsed
 def equal_ignore_order(a, b):
     unmatched = list(b)
     for element in a:
